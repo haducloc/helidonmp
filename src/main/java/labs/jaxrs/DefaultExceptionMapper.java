9d982be6486eea0448c0217a10ff6e13d10926f9
Loc Ha <loc.ha@nebraska.gov>
2023-05-26 12:36:49 -0500

    Changes

--- src/main/java/labs/jaxrs/DefaultExceptionMapper.java
+++ src/main/java/labs/jaxrs/DefaultExceptionMapper.java
@@ -2,6 +2,7 @@ package labs.jaxrs;
 
 import com.appslandia.common.utils.ExceptionUtils;
 
+import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.ExceptionMapper;
@@ -14,8 +15,15 @@ public class DefaultExceptionMapper implements ExceptionMapper<Exception> {
 	@Override
 	public Response toResponse(Exception ex) {
 
-		return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+		if (ex instanceof WebApplicationException wae) {
+			return Response.status(wae.getResponse().getStatus())
+			.entity(new Result().asError().message(ExceptionUtils.buildMessage(ex)))
+			.type(MediaType.APPLICATION_JSON).build();
+
+		} else {
+			return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
 				.entity(new Result().asError().message(ExceptionUtils.buildMessage(ex)))
 				.type(MediaType.APPLICATION_JSON).build();
+		}		
 	}
 }
